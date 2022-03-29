b641738e4c760f6e837c382a41b6253ebbfd2ec5
derrick_ke <derrickselempo@gmail.com>
2022-03-29 18:29:52 +0300

    Added search and clear functionality

--- src/components/context/github/GithubContext.jsx
+++ src/components/context/github/GithubContext.jsx
@@ -14,27 +14,42 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       header: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
 
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
+  const clearUsers = () =>
+    dispatch({
+      type: 'CLEAR_USERS',
+    });
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
